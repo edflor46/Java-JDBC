5f19464f5f1c0fceec520aa84a68b649e566cf49
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-10-03 11:04:29 -0500

    CRUD: Insert

--- src/main/java/test/testManejoPersonas.java
+++ src/main/java/test/testManejoPersonas.java
@@ -8,12 +8,16 @@ import java.util.List;
 public class testManejoPersonas {
     public static void main(String[] args) {
         PersonaDAO personaDAO = new PersonaDAO();
+
+        //Insertando un nuevo objeto de tipo persona
+        Persona personaNueva = new Persona("Luis Eduardo", "Flores", "test6@test.com", "55443322");
+        personaDAO.insertar(personaNueva);
+
+
         List<Persona> personas = personaDAO.seleccionar();
 
         for (Persona persona : personas){
             System.out.println("Persona: " + persona );
         }
-
-
     }
 }
