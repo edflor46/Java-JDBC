18b8f4252fd25c30eaa5f163ee039c0164769971
Eduardo Flores <luis.flores.ramirez@accenture.com>
2021-11-11 16:26:06 -0600

    Clase Usuario Agregada

---
+++ src/main/java/domain/Usuarios.java
@@ -0,0 +1,54 @@
+package domain;
+
+public class Usuarios {
+
+    private int id_usuario;
+    private String usuario;
+    private String password;
+
+    public Usuarios() {
+    }
+
+    public Usuarios(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public Usuarios(int id_usuario, String usuario, String password) {
+        this.id_usuario = id_usuario;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuarios{" +
+                "id_usuario=" + id_usuario +
+                ", usuario='" + usuario + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
